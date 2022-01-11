29b6438a67812101b3bb4f0d2c5037f9f2289a8a
Felipe <felipe.507@hotmail.com>
2022-01-11 11:09:08 -0300

    autenticacao-boostrap

---
+++ resources/views/teste.blade.php
@@ -0,0 +1,15 @@
+
+<!doctype html>
+<html>
+    <head>
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+
+        <h1>This is example Bootstrap page</h1>
+    </body>
+</html>
